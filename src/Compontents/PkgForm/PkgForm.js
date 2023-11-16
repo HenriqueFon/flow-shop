3148fa2beedb132e37ac5ade1196bd5a8e899f55
henriqueFon <henriquearaujo091@gmail.com>
2023-11-16 00:16:18 -0300

    adding error treatment and buttons for optimize, randomize packages

--- src/Compontents/PkgForm/PkgForm.js
+++ src/Compontents/PkgForm/PkgForm.js
@@ -12,9 +12,8 @@ export function PkgForm() {
     const [instalationTime, setInstalationTime] = useState("")
     const [packageNumber, setPackageNumber] = useState(0)
     const [showResults, setShowResults] = useState(false);
-    const [showCreatedPackages, setshowCreatedPackages] = useState(true);
+    const [showCreatedPackages, setShowCreatedPackages] = useState(true);
 
-    console.log(packages)
 
     const handleDependencies = (event) => {
         setDependencies(event.target.value);
@@ -29,66 +28,132 @@ export function PkgForm() {
     }
 
     const handleSubmit = (event) => {
+        if(validateMaxPackages()) {
+            event.preventDefault();
+
+            const newPackage = {
+                "Dependencies": dependencies,
+                "Installation_Time": instalationTime,
+                "Package": `Package_${packageNumber}`,
+                "Package_Size": packageSize
+            };
+
+            let number = packageNumber + 1;
+
+            setPackageNumber(number);
+
+            setPackages([...packages, newPackage]);
+
+            // Clear the form fields
+            // setDependencies("");
+            // setPackageSize("");
+            // setInstalationTime("");
+        }
+    }
+
+    const handleRandomSubmit = (event) => {
+
+        if(validateMaxPackages()) {
+            event.preventDefault();
+
+            const randomDependencies = Math.floor(Math.random() * 10) 
+            + 1; // Random value between 1 and 10
+            const randomPackageSize = Math.floor(Math.random() * 91) 
+            + 10; // Random value between 10 and 100
+            const randomInstallationTime = Math.floor(Math.random() * 91) 
+            + 10; // Random value between 10 and 100
+
+            const newPackage = {
+                "Dependencies": randomDependencies,
+                "Installation_Time": randomPackageSize,
+                "Package": `Package_${packageNumber}`,
+                "Package_Size": randomInstallationTime
+            };
+
+            let number = packageNumber + 1;
+
+            setPackageNumber(number);
+
+            setPackages([...packages, newPackage]);
+
+            // Clear the form fields
+            // setDependencies("");
+            // setPackageSize("");
+            // setInstalationTime("");
+        }
+    }
+
+    const validateMaxPackages = () => {
+        if(packages.length < 10) {
+            return true;
+        }
+
+        return false;
+    }
+
+    const handleOptimize = (event) => {
         event.preventDefault();
 
-        const newPackage = {
-            "Dependencies": dependencies,
-            "Installation_Time": instalationTime,
-            "Package": `Package_${packageNumber}`,
-            "Package_Size": packageSize
-        };
+        setShowCreatedPackages(false)
+        setShowResults(true)
+    }
 
-        let number = packageNumber + 1;
-
-        setPackageNumber(number);
-
-        setPackages([...packages, newPackage]);
-
-        // Clear the form fields
-        // setDependencies("");
-        // setPackageSize("");
-        // setInstalationTime("");
+    const handleBack = (event) => {
+        event.preventDefault();
+        setPackages("");
+        setShowResults(false);
+        setShowCreatedPackages(true)
     }
 
     return (
         <>
-        <div className = 'packageForm'>
+        { showResults == false && <div className = 'packageForm'>
                 <form onSubmit = {handleSubmit}>
                     <div>
                         <label for = "dependencies">Dependências</label>
-                        <input type = "text"
+                        <input type = "number"
                             name = "dependencies"
                             onChange = {handleDependencies}
-                        />
+                        required/>
                     </div>
                     <div>
                         <label for = "size">Tamanho</label>
-                        <input type = "text"
+                        <input type = "number"
                             name = "size"
                             onChange = {handleSize}
-                        />
+                        required/>
                     </div>
                     <div>
                         <label for = "time">Tempo</label>
-                        <input type = "text"
+                        <input type = "number"
                             name = "time"
                             onChange = {handleTime}
-                        />
+                        required/>
                     </div>
                     <button>Criar pacote</button>
                 </form>
                 <div className = "randomButton">
-                    <input type = "button"value = "Aleatório"></input>
+                    <input type = "button" 
+                    value = "Aleatório" 
+                    onClick = {handleRandomSubmit}></input>
                 </div>
                 <div className = "OptimizeButton">
-                    <input type = "button" value = "Otimizar"></input>
+                    <input type = "button" 
+                    value = "Otimizar" 
+                    onClick = {handleOptimize}></input>
                 </div>
+        </div> }
+        {packages.length == 10 && <p>Limite de pacotes excedido, máximo 10</p>}
+        <div>
+            { showCreatedPackages == true && packages.length != 0 && <PrePackageTable data = { packages }/> }
         </div>
         <div>
-            { showCreatedPackages == true && <PrePackageTable data = { packages }/> }
+            { showResults == true && <Result packages = { packages }/> }
         </div>
         <div>
-            { showResults == true && <Result /> }
+            { showResults == true && <input type = "button" 
+            value = "Voltar" 
+            onClick = {handleBack}></input> }
         </div>
         </>
     )   
